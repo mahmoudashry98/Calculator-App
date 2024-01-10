d2a7fcf8aa92f97cf950ad3458fbd3b1957de6f7
mahmoudashry98 <83143927+mahmoudashry98@users.noreply.github.com>
2024-01-10 20:58:53 +0200

    second commit

---
+++ lib/app/core/theme/app_color.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+const Color kMainColor = Color(0xFF4D63EF);
+const Color kMainColorDark = Color(0xFF00264c);
+const Color kAccentColor = Color(0xFF4D515E);
+const Color kWhiteColor = Color(0xFFFFFFFF);
+const Color kBlackColor = Color(0xFF19181E);
+const Color kGrayColor = Color(0xFF3C3B41);
+const Color kCardBackgroundColor = Color(0xfff8f9fd);
+const Color kCardBackgroundDarkColor = Color(0xFF423F55);
+const Color kScaffoldBackgroundColor = Color(0xFFF2F2F2);
+
+const Color kScaffoldDarkBackgroundColor = Color(0xFF010D1A);
+const Color kTextColor = Color(0xff82858A);
+const Color kMainTextColor = Color(0xFF121112);
+const Color kMainColorLight = Color(0xFF423F55);
+const Color kLightTextColor = Colors.grey;
+// const Color kLightTextColor = Colors.grey;
---
+++ lib/app/core/theme/app_theme.dart
@@ -0,0 +1,173 @@
+import 'package:calculator_app/app/core/theme/app_color.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+final ThemeData appTheme = ThemeData(
+    scaffoldBackgroundColor: kScaffoldBackgroundColor,
+    secondaryHeaderColor: kMainTextColor,
+    primaryColor: kMainColor,
+    primaryColorDark: kMainColorDark,
+    primaryColorLight: kMainColorLight,
+    dividerColor: const Color(0xFFD4D4D4),
+    cardColor: kCardBackgroundColor,
+    hintColor: kLightTextColor,
+    indicatorColor: kMainColor,
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        textStyle: const TextStyle(
+          fontSize: 16,
+          color: kMainTextColor,
+        ),
+      ),
+    ),
+    inputDecorationTheme: InputDecorationTheme(
+      hintStyle: TextStyle(
+        //fontFamily: CommonVariables.settings.read(FONT_FAMILY),
+        fontSize: 13,
+        color: const Color(0xffa0a0a0),
+      ),
+      labelStyle: TextStyle(
+        //fontFamily: CommonVariables.settings.read(FONT_FAMILY),
+        fontSize: 14,
+        fontWeight: FontWeight.bold,
+        //   color: const Color(0xff000000),
+      ),
+      alignLabelWithHint: true,
+      focusedBorder: const OutlineInputBorder(
+        borderSide: BorderSide(
+          color: kMainColor,
+        ),
+      ),
+      border: const OutlineInputBorder(
+        borderSide: BorderSide(
+          color: Color(0xffa0a0a0),
+        ),
+      ),
+    ),
+    tabBarTheme: const TabBarTheme(
+        labelStyle: TextStyle(
+            fontSize: 16, color: kMainTextColor, fontWeight: FontWeight.bold),
+        unselectedLabelStyle: TextStyle(
+          fontSize: 16,
+          color: kMainTextColor,
+        )),
+    appBarTheme: const AppBarTheme(
+        backgroundColor: kScaffoldBackgroundColor,
+        elevation: 1,
+        shadowColor: kMainColor,
+        centerTitle: true,
+        systemOverlayStyle: SystemUiOverlayStyle(
+          statusBarColor: Colors.transparent,
+          statusBarIconBrightness: Brightness.dark,
+        ),
+        toolbarTextStyle: TextStyle(
+          fontSize: 16,
+          fontWeight: FontWeight.w700,
+        ),
+        titleTextStyle: TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 18.3,
+          color: kMainTextColor,
+          fontFamily: 'Cairo',
+        ),
+        iconTheme: IconThemeData(color: kMainTextColor),
+        actionsIconTheme: IconThemeData(
+          color: kMainColor,
+        )),
+    floatingActionButtonTheme: const FloatingActionButtonThemeData(
+      backgroundColor: kScaffoldDarkBackgroundColor,
+      elevation: 1,
+    ),
+    iconTheme: const IconThemeData(color: kMainTextColor),
+    primaryIconTheme: const IconThemeData(color: kMainTextColor),
+    colorScheme: const ColorScheme.light(primary: kMainColor),
+    textSelectionTheme: const TextSelectionThemeData(cursorColor: kMainColor),
+    sliderTheme: const SliderThemeData(
+      trackHeight: 2,
+      thumbShape: RoundSliderThumbShape(enabledThumbRadius: 12.0),
+    ));
+final ThemeData appDarkTheme = ThemeData(
+  scaffoldBackgroundColor: kScaffoldDarkBackgroundColor,
+  secondaryHeaderColor: kMainTextColor,
+  primaryColor: kMainColor,
+  primaryColorDark: kMainColorDark,
+  primaryColorLight: kMainColorLight,
+  cardColor: kCardBackgroundDarkColor,
+  dividerColor: const Color(0xFFD4D4D4),
+  textButtonTheme: TextButtonThemeData(
+    style: ElevatedButton.styleFrom(
+      textStyle: const TextStyle(color: kWhiteColor),
+    ),
+  ),
+  indicatorColor: kMainColor,
+  inputDecorationTheme: InputDecorationTheme(
+    hintStyle: TextStyle(
+      // fontFamily: CommonVariables.settings.read(FONT_FAMILY),
+      fontSize: 13,
+    ),
+    labelStyle: TextStyle(
+      // fontFamily: CommonVariables.settings.read(FONT_FAMILY),
+      fontSize: 14,
+      fontWeight: FontWeight.bold,
+      color: const Color(0xffffffff),
+    ),
+    alignLabelWithHint: true,
+    focusedBorder: const OutlineInputBorder(
+      borderSide: BorderSide(
+        color: Color(0xffa0a0a0),
+      ),
+    ),
+    border: const OutlineInputBorder(
+      borderSide: BorderSide(
+        color: Color(0xffa0a0a0),
+      ),
+    ),
+  ),
+  elevatedButtonTheme: ElevatedButtonThemeData(
+    style: ElevatedButton.styleFrom(
+      textStyle: const TextStyle(
+        fontSize: 16,
+        color: kWhiteColor,
+      ),
+    ),
+  ),
+  tabBarTheme: const TabBarTheme(
+      labelStyle: TextStyle(
+          fontSize: 16, color: Colors.white, fontWeight: FontWeight.bold),
+      unselectedLabelStyle: TextStyle(
+        fontSize: 16,
+        color: Colors.white,
+      )),
+  floatingActionButtonTheme: const FloatingActionButtonThemeData(
+    backgroundColor: kWhiteColor,
+    elevation: 1,
+  ),
+  appBarTheme: const AppBarTheme(
+      backgroundColor: kScaffoldDarkBackgroundColor,
+      elevation: 0.0,
+      centerTitle: true,
+      systemOverlayStyle: SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent,
+        statusBarIconBrightness: Brightness.light,
+      ),
+      toolbarTextStyle: TextStyle(
+        fontSize: 16,
+        fontWeight: FontWeight.w700,
+      ),
+      titleTextStyle: TextStyle(
+        fontWeight: FontWeight.bold,
+        fontSize: 18.3,
+        color: Colors.white,
+        fontFamily: 'Cairo',
+      ),
+      iconTheme: IconThemeData(color: Colors.white),
+      actionsIconTheme: IconThemeData(color: kWhiteColor)),
+  colorScheme: const ColorScheme.dark(primary: kMainColor),
+  iconTheme: const IconThemeData(color: Colors.white),
+  primaryIconTheme: const IconThemeData(color: Colors.white),
+  textSelectionTheme: const TextSelectionThemeData(cursorColor: kMainColor),
+  sliderTheme: const SliderThemeData(
+    trackHeight: 2,
+    thumbShape: RoundSliderThumbShape(enabledThumbRadius: 12.0),
+  ),
+);
